0a8aeb880e3ef8106702c16ddf743936c0adf7cb
Lasse Arkko <lasse.arkko@gmail.com>
2018-01-20 13:31:28 +0200

    Change keyboard state on MIDI events

--- src/gui/mainwindow.cc
+++ src/gui/mainwindow.cc
@@ -3,7 +3,20 @@
 #include "mainwindow.hh"
 
 Main_window::Main_window()
-    : m_midi_input(MIDI_input([](MIDI_input::Event e){(void)e;}))
+    : m_midi_input(MIDI_input
+    (
+        [&](MIDI_input::Event e)
+        {
+            if(e.down)
+            {
+                m_keyboard.set(e.key, e.velocity);
+            }
+            else
+            {
+                m_keyboard.set(e.key, 0.0f);
+            }
+        }
+    ))
 {
     
 }
