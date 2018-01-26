6859abe8be917d27f589ce50aa5c26b342d8f4f8
Lasse Arkko <lasse.arkko@gmail.com>
2018-01-26 22:15:28 +0200

    Add ability to play notes with mouse in keyboard widget

--- src/gui/keyboardwidget.cc
+++ src/gui/keyboardwidget.cc
@@ -12,22 +12,65 @@ Keyboard_widget::Keyboard_widget(Keyboard & keyboard, QWidget * parent)
 {
 }
 
+QRect Keyboard_widget::key_rect(int key)
+{
+    int width = ceil((float)(this->width()) / (float)(m_keyboard.key_count()));
+    int height = this->height();
+    int x = floor(key * width);
+    int y = 0;
+    return QRect(x, y, width, height);
+}
+
 void Keyboard_widget::paintEvent(QPaintEvent * event)
 {
     QPainter painter(this);
-    for(size_t i = 0; i < m_keyboard.key_count(); ++i)
+    for(size_t key = 0; key < m_keyboard.key_count(); ++key)
     {
         /*Draw keys*/
-        Qt::GlobalColor color = m_keyboard.key_is_active(i)
+        Qt::GlobalColor color = m_keyboard.key_is_active(key)
                               ? Qt::cyan
                               : Qt::darkGray;
-        float width = (float)(this->width()) / (float)(m_keyboard.key_count());
-        int height = this->height();
-        int x = floor(i * width);
-        painter.fillRect(x, 0, ceil(width), height, color);
+        QRect rect = key_rect(key);
+        painter.fillRect(rect, color);
         /*Draw separator between keys*/
         Qt::GlobalColor separator_color = Qt::black;
         painter.setPen(separator_color);
-        painter.drawLine(x, 0, x, height);
+        painter.drawLine(rect.topLeft(), rect.bottomLeft());
     }
 }
+
+int Keyboard_widget::key_at(QPoint location)
+{
+    for(size_t key = 0; key < m_keyboard.key_count(); ++key)
+    {
+        QRect rect = key_rect(key);
+        if(location.x() >= rect.x() && location.x() < (rect.x() + rect.width()))
+        {
+            return key;
+        }
+    }
+    return -1;
+}
+
+
+void Keyboard_widget::mousePressEvent(QMouseEvent * event)
+{
+    int key = key_at(event->pos());
+    if(key >= 0)
+    {
+        if(m_keyboard.key_is_active(key))
+        {
+            m_keyboard.set(key, Keyboard::Keypress(0.0f));
+        }
+        else
+        {
+            m_keyboard.set(key, Keyboard::Keypress(1.0f));
+        }
+        update();
+    }
+}
+
+
+
+
+
--- src/gui/keyboardwidget.hh
+++ src/gui/keyboardwidget.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QWidget>
+#include <QRect>
+#include <QMouseEvent>
 
 #include "keyboard.hh"
 
@@ -10,6 +12,9 @@ class Keyboard_widget : public QWidget
   public:
     Keyboard_widget(Keyboard & keyboard, QWidget * parent = nullptr);
   private:
+    QRect key_rect(int key);
     void paintEvent(QPaintEvent * event) override;
+    int key_at(QPoint location);
+    void mousePressEvent(QMouseEvent * event) override;
     Keyboard & m_keyboard;
 };
