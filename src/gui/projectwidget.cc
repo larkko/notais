638a9da3dadaefebebee290fdf447fb41ea30620
Lasse Arkko <lasse.arkko@gmail.com>
2018-02-03 18:12:57 +0200

    Properly align instruments/tunings labels in Project_widget

--- src/gui/projectwidget.cc
+++ src/gui/projectwidget.cc
@@ -10,6 +10,7 @@ Project_widget::Project_widget()
     QVBoxLayout * instrument_side_layout = new QVBoxLayout();
     QLabel * instruments_label = new QLabel();
     instruments_label->setText("Instruments");
+    instruments_label->setAlignment(Qt::AlignHCenter);
     instrument_side_layout->addWidget(instruments_label);
     m_instrument_list_layout = new QVBoxLayout();
     instrument_side_layout->addLayout(m_instrument_list_layout);
@@ -17,6 +18,7 @@ Project_widget::Project_widget()
     QVBoxLayout * tuning_side_layout = new QVBoxLayout();
     QLabel * tunings_label = new QLabel();
     tunings_label->setText("Tunings");
+    tunings_label->setAlignment(Qt::AlignHCenter);
     tuning_side_layout->addWidget(tunings_label);
     m_tuning_list_layout = new QVBoxLayout();
     tuning_side_layout->addLayout(m_tuning_list_layout);
