31a5d18110dfd7437b56777f4b30b91ece6d2b22
Lasse Arkko <lasse.arkko@gmail.com>
2018-02-03 18:21:34 +0200

    Make instruments/tunings labels in Project_widget more prominent

--- src/gui/projectwidget.cc
+++ src/gui/projectwidget.cc
@@ -11,6 +11,10 @@ Project_widget::Project_widget()
     QLabel * instruments_label = new QLabel();
     instruments_label->setText("Instruments");
     instruments_label->setAlignment(Qt::AlignHCenter);
+    QFont label_font = instruments_label->font();
+    label_font.setPointSize(13);
+    label_font.setBold(true);
+    instruments_label->setFont(label_font);
     instrument_side_layout->addWidget(instruments_label);
     m_instrument_list_layout = new QVBoxLayout();
     instrument_side_layout->addLayout(m_instrument_list_layout);
@@ -19,6 +23,7 @@ Project_widget::Project_widget()
     QLabel * tunings_label = new QLabel();
     tunings_label->setText("Tunings");
     tunings_label->setAlignment(Qt::AlignHCenter);
+    tunings_label->setFont(label_font);
     tuning_side_layout->addWidget(tunings_label);
     m_tuning_list_layout = new QVBoxLayout();
     tuning_side_layout->addLayout(m_tuning_list_layout);
