9c0edaed5e41d0e11c254d1c5555702f6919d58a
Lasse Arkko <lasse.arkko@gmail.com>
2018-02-14 00:03:49 +0200

    Make Adjustable_audio_source depend on an Audio_source

--- src/lib/audio/adjustableaudiosource.cc
+++ src/lib/audio/adjustableaudiosource.cc
@@ -1,10 +1,18 @@
 #include "adjustableaudiosource.hh"
 
+Adjustable_audio_source::Adjustable_audio_source
+(
+    std::shared_ptr<Audio_source> const & audio_source
+)   : m_audio_source(audio_source)
+{
+
+}
+
 Audio_data::Sample Adjustable_audio_source::get_sample
 (
     float frequency,
     double offset
 ) const
 {
-    return 0.0f;
+    return m_audio_source->get_sample(frequency, offset);
 }
--- src/lib/audio/adjustableaudiosource.hh
+++ src/lib/audio/adjustableaudiosource.hh
@@ -1,14 +1,21 @@
 #pragma once
 
+#include <memory>
+
 #include "audiosource.hh"
 
 class Adjustable_audio_source : public Audio_source
 {
   public:
+    Adjustable_audio_source
+    (
+        std::shared_ptr<Audio_source> const & audio_source
+    );
     virtual Audio_data::Sample get_sample
     (
         float frequency,
         double offset
     ) const override;
   private:
+    std::shared_ptr<Audio_source> m_audio_source;
 };
