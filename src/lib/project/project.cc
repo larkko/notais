9f9ceb25809ef31f26995d8fc58885cb1233f18e
Lasse Arkko <lasse.arkko@gmail.com>
2018-02-14 01:07:17 +0200

    Add methods to manipulate instruments and tunings in project

--- src/lib/project/project.cc
+++ src/lib/project/project.cc
@@ -1,1 +1,26 @@
 #include "project.hh"
+
+
+void Project::add_instrument
+(
+    std::shared_ptr<Adjustable_audio_source> const & instrument
+)
+{
+    m_instruments.push_back(instrument);
+}
+
+std::vector<std::shared_ptr<Adjustable_audio_source>> const &
+Project::instruments() const
+{
+    return m_instruments;
+}
+
+void Project::add_tuning(std::shared_ptr<Tuning> const tuning)
+{
+    m_tunings.push_back(tuning);
+}
+
+std::vector<std::shared_ptr<Tuning>> const & Project::tunings() const
+{
+    return m_tunings;
+}
--- src/lib/project/project.hh
+++ src/lib/project/project.hh
@@ -9,6 +9,14 @@
 class Project
 {
   public:
+    void add_instrument
+    (
+        std::shared_ptr<Adjustable_audio_source> const & instrument
+    );
+    std::vector<std::shared_ptr<Adjustable_audio_source>> const & 
+    instruments() const;
+    void add_tuning(std::shared_ptr<Tuning> const tuning);
+    std::vector<std::shared_ptr<Tuning>> const & tunings() const;
   private:
     std::vector<std::shared_ptr<Adjustable_audio_source>> m_instruments;
     std::vector<std::shared_ptr<Tuning>> m_tunings;
