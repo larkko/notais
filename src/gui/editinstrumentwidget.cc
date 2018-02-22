71374c94106f0c4d4d9aecfd5039bc359c6611d6
Lasse Arkko <lasse.arkko@gmail.com>
2018-02-22 10:31:04 +0200

    Use tabs for Edit_instrument_widget

--- src/gui/editinstrumentwidget.cc
+++ src/gui/editinstrumentwidget.cc
@@ -2,6 +2,7 @@
 
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QTabWidget>
 
 Edit_instrument_widget::Edit_instrument_widget
 (
@@ -10,10 +11,17 @@ Edit_instrument_widget::Edit_instrument_widget
 )
     : QWidget(parent)
 {
-    QVBoxLayout * layout = new QVBoxLayout;
+    QVBoxLayout * layout = new QVBoxLayout();
 
-    QLabel * label = new QLabel("edit instrument widget");
-    layout->addWidget(label);
+    QTabWidget * tabs = new QTabWidget();
+    layout->addWidget(tabs);
+
+    QLabel * general_tab = new QLabel("general tab");
+    tabs->addTab(general_tab, "general");
+    QLabel * instrument_tab = new QLabel("instrument tab");
+    tabs->addTab(instrument_tab, "instrument");
+    QLabel * effects_tab = new QLabel("effects tab");
+    tabs->addTab(effects_tab, "effects");
 
     this->setLayout(layout);
 }
