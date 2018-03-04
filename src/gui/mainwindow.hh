7dc5141bf0578c2dd9dce437ec749c4c2463a9c1
Lasse Arkko <lasse.arkko@gmail.com>
2018-03-04 23:19:33 +0200

    Make active instrument and tuning shared_ptrs to const in main window

--- src/gui/mainwindow.hh
+++ src/gui/mainwindow.hh
@@ -23,8 +23,8 @@ class Main_window : public QWidget
     Keyboard m_keyboard;
     Audio_output m_audio_out;
     QTimer * m_audio_stop_timer;
-    std::shared_ptr<Adjustable_audio_source> m_active_instrument;
-    std::shared_ptr<Tuning> m_active_tuning;
+    std::shared_ptr<Adjustable_audio_source const> m_active_instrument;
+    std::shared_ptr<Tuning const> m_active_tuning;
   signals:
     void keyboard_state_changed();
 };
