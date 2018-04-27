99cb6b1125f16f1cd6d2c8334bcd65507f6f51b6
Lasse Arkko <lasse.arkko@gmail.com>
2018-04-27 22:15:37 +0300

    Remove separate function for Oscillator's square and saw waves

--- src/lib/audio/oscillator.cc
+++ src/lib/audio/oscillator.cc
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
 
 #include "oscillator.hh"
 
@@ -11,29 +12,6 @@ Oscillator::Oscillator(Oscillator::Type type)
     : m_type(type)
 {
 
-}
-
-Audio_data::Sample square_saw_at
-(
-    double wave_offset,
-    Oscillator::Type type
-)
-{
-    Audio_data::Sample sample = 0.0f;
-    float resulting_amplitude = 0.0f;
-    /*Exact number doesn't matter, just stack enough to approximate
-      a decent saw/square wave*/
-    int const harmonic_count = 15;
-    /*For square waves, skip every even harmonic*/
-    int const harmonic_step = (type == Oscillator::Type::Saw)
-                            ? 1
-                            : 2; 
-    for(int harmonic = 1; harmonic < harmonic_count; harmonic += harmonic_step)
-    {
-        sample += sinf(wave_offset * harmonic);
-        resulting_amplitude += 1.0f / harmonic;
-    }
-    return Audio_data::max_amplitude * sample / resulting_amplitude;
 }
 
 Audio_data::Sample random_sample()
@@ -64,8 +42,23 @@ Audio_data::Sample Oscillator::get_sample
             sample = random_sample();
             break;
         default:
-            sample = square_saw_at(wave_offset, m_type);
-            break;
+        {
+            float resulting_amplitude = 0.0f;
+            Audio_data::Sample layered_sample = 0.0f;
+            /*Exact number doesn't matter, just stack enough to approximate
+              a decent saw/square wave*/
+            int const harmonic_count = 15;
+            /*For square waves, skip every even harmonic*/
+            int const harmonic_step = (m_type == Oscillator::Type::Saw)
+                                    ? 1
+                                    : 2; 
+            for(int harmonic = 1; harmonic < harmonic_count; harmonic += harmonic_step)
+            {
+                layered_sample += sinf(wave_offset * harmonic);
+                resulting_amplitude += 1.0f / harmonic;
+            }
+            sample = Audio_data::max_amplitude * layered_sample / resulting_amplitude;
+        } break;
     }
     
     return sample;
